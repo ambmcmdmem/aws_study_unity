5dd9ac0928e511c7efa3bc27a075cc3e1d374e12
root <root@ip-172-31-36-184.ap-northeast-1.compute.internal>
2021-08-18 09:51:46 +0000

    laravel/uiの導入・HomeControllerの導入

--- aws_study_unity/routes/web.php
+++ aws_study_unity/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/about', function () {
+//     return "Hi about page";
+// });
+
+// Route::get('/contact', function () {
+//     return "contact";
+// });
+
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return 'This is post number ' . $id . ' ' . $name;
+// });
+
+// Route::get('/admin/post/examples',array('as' => 'admin.post' , function () {
+//     return "This url " . route('admin.post');
+// }));
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('/post/{id}', [App\Http\Controllers\PostController::class, 'index']);
+Route::get('/posts/contact', [App\Http\Controllers\PostController::class, 'contact']);
+Route::get('/posts/{id}/{name}/{password}', [App\Http\Controllers\PostController::class, 'show_post']);
+// Route::resource('posts', App\Http\Controllers\PostController::class);
