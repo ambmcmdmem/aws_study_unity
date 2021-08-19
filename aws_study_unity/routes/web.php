cad895cf8cd807d97faa24d70d56b382a3854959
root <root@ip-172-31-36-184.ap-northeast-1.compute.internal>
2021-08-19 04:33:54 +0000

    RouteからのDB操作について学習

--- aws_study_unity/routes/web.php
+++ aws_study_unity/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/about', function () {
 //     return "Hi about page";
@@ -41,3 +42,105 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/posts/contact', [App\Http\Controllers\PostController::class, 'contact']);
 Route::get('/posts/{id}/{name}/{password}', [App\Http\Controllers\PostController::class, 'show_post']);
 // Route::resource('posts', App\Http\Controllers\PostController::class);
+
+Route::get('/insert', function() {
+
+    // モデルなし
+    // DB::insert('insert into posts(title, contents) values(?, ?)', ['First title!', 'Laravel is very useful!!!']);
+    
+    // モデル指定
+    // Post::insert([
+    //     'title'     =>  'Second title...',
+    //     'contents'  =>  'Learning PHP is very fun!'
+    // ]);
+
+});
+
+// Route::get('/read', function() {
+
+//     $results = DB::select('select * FROM posts WHERE id = ?', [1]);
+//     dd($results);
+// });
+
+// Route::get('/update', function() {
+
+//     $updated = DB::update('UPDATE posts SET title = "update_title", contents = "update body" WHERE id=?', [1]);
+
+// });
+
+Route::get('/find', function() {
+
+
+    // $posts = Post::all();
+
+    // // dd($posts);
+    // $post_titles_str = '';
+    // foreach($posts as $post) {
+    //     $post_titles_str .= $post->title . "<br>";
+    // }
+    // return $post_titles_str;
+
+    // $post = Post::find(1);
+
+    // dd($post);
+
+});
+
+// Route::get('/findwhere', function() {
+
+
+
+//     $posts = Post::where('id', '>=', 1)->get();
+
+//     dd($posts);
+
+
+// });
+
+// Route::get('/basicinsert', function() {
+//     $post = Post::find(2);
+//     $post->title = 'Nice title 2';
+//     $post->contents = 'This is content! 2';
+//     $post->save();
+// });
+
+Route::get('/create', function() {
+    Post::create([
+        'title'     =>  'PHP',
+        'contents'  =>  'It is a content'
+    ]);
+});
+
+// Route::get('/update', function() {
+//     Post::where('id', 2)
+//         ->where('is_admin', 0)
+//         ->update([
+//             'title' => 'Hello Update',
+//             'contents' => 'Hello Contents Update'
+//         ]);
+// });
+
+// Route::get('/delete', function() {
+//     Post::where('id', 2)->delete();
+// });
+
+// Route::get('/softdelete', function() {
+//     Post::find(2)->delete();
+// });
+
+// Route::get('/readsoftdelete', function() {
+//     //$post = Post::find(1);
+
+//     // onlytrashedはソフトデリートされたモデルのみ、withtrashedはデリーとされていないモデルも出力
+//     $post = Post::onlyTrashed()->where('id', 1)->first();
+//     dd($post);
+
+// });
+
+// Route::get('/restore', function() {
+//     Post::withTrashed()->where('is_admin', 0)->restore();
+// });
+
+// Route::get('/forcedelete', function() {
+//     Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
+// });
